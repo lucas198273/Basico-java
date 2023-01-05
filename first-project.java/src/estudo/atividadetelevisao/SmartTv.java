81fcdd782ba94a255e85b3a7a68e25aa5837dcbc
Lucas Dias <lucasinga2000@gmail,com>
2023-01-05 20:38:57 -0300

    "smartTv "

---
+++ first-project.java/src/estudo/atividadetelevisao/SmartTv.java
@@ -0,0 +1,39 @@
+package estudo.atividadetelevisao;
+
+public class SmartTv {
+    boolean ligada = false;
+    int canal = 1;
+    int volume = 25;
+
+    public void ligar(){
+        ligada = true;
+        System.out.println("Status : Televeisao Ligada");
+    }
+    public void desligar(){
+        ligada = false;
+        System.out.println("Status:  Televisao Desligada");
+    }
+    public void aumentarVolume(){
+        //volume = volume +1;
+        volume++;
+        System.out.println("Volume Aumentado para: " + volume);
+    }
+    public void diminuirVolume(){
+        //volume = volume -1;
+        volume--;
+        System.out.println("Volume Dimunuido para: "+ volume);
+    }
+    public void mudarCanal(int novoCanal){
+          canal = novoCanal;
+          System.out.println("Canal Atual: "+ novoCanal);
+    }
+    public void aumanetarCanal(){
+        canal++;
+        System.out.println("Canal atual: " + canal);
+    }
+    public void diminuirCanal(){
+        canal--;
+        System.out.println("Canal atual: "+ canal);
+    }
+
+}
---
+++ first-project.java/src/estudo/atividadetelevisao/Usuario.java
@@ -0,0 +1,39 @@
+package estudo.atividadetelevisao;
+
+public class Usuario {
+
+    public static void main(String[] args) throws Exception{
+        SmartTv smartTv = new SmartTv();
+        
+        
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        System.out.println("volume atual :" +smartTv.volume);
+
+        smartTv.aumentarVolume();
+        smartTv.aumentarVolume();
+        smartTv.aumentarVolume();
+        System.out.println("volume atual: "+ smartTv.volume);
+
+        System.out.println("TV ligada: " + smartTv.ligada);
+        System.out.println("canal atual: "+ smartTv.volume);
+        System.out.println("Canal atual: "+ smartTv.canal);
+     
+        smartTv.ligar();
+        System.out.println("Novo Status => TV Ligada ?" + smartTv.ligada);
+
+        smartTv.desligar();
+        System.out.println("Novo Status => TV Ligada?" + smartTv.ligada);
+        
+        smartTv.mudarCanal(5);
+        smartTv.mudarCanal(6);
+        smartTv.mudarCanal(8);
+
+        smartTv.desligar();
+        smartTv.ligar();
+        smartTv.mudarCanal(87);
+    } 
+
+
+
+}
